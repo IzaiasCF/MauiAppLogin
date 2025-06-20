e67b6b5f9ac7a9dd4f5520d3339a0e27e450c28a
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-20 16:03:46 -0300

    add commit

--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -2,52 +2,52 @@ namespace MauiAppLogin;
 
 public partial class Login : ContentPage
 {
-	public Login()
-	{
-		InitializeComponent();
-	}
+    public Login()
+    {
+        InitializeComponent();
+    }
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
-		try
-		{
-			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>()
-			{
-					new DadosUsuario()
-					{
-						Usuario = "José",
-						Senha = "123"
-					},
-					new DadosUsuario()
-					{
-						Usuario = "Maria",
-						Senha = "321"
-					}
-			};
+        try
+        {
+            List<DadosUsuario> lista_usuarios = new List<DadosUsuario>()
+            {
+                    new DadosUsuario()
+                    {
+                        Usuario = "jose",
+                        Senha = "123"
+                    },
+                    new DadosUsuario()
+                    {
+                        Usuario = "maria",
+                        Senha = "321"
+                    }
+            };
 
-			DadosUsuario dados_digitados = new DadosUsuario()
-			{
+            DadosUsuario dados_digitados = new DadosUsuario()
+            {
                 Usuario = txt_usuario.Text,
                 Senha = txt_senha.Text
 
             };
 
-			// LINQ
-			if (lista_usuarios.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha)))
-			{
-				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+            // LINQ
+            if (lista_usuarios.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha)))
+            {
+                await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
 
-				App.Current.MainPage = new Protegida();
+                App.Current.MainPage = new Protegida();
 
-			} else 
-			{
-				throw new Exception("Usuário ou senha Incorretos!");
-			}
-		}
+            } else 
+            {
+                throw new Exception("Usuário ou senha Incorretos!");
+            }
+        }
 
-		catch (Exception ex)
-		{
-			await DisplayAlert("Ops!", ex.Message, "Fechar");
-		}
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "Fechar");
+        }
     }
 }
