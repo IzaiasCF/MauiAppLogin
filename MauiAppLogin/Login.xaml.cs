e12e6e91785153286826fd74a7dc110d8d87fd47
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-20 11:43:13 -0300

    add commit

--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -9,6 +9,38 @@ public partial class Login : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
+		try
+		{
+			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>()
+		    { 
+					new DadosUsuario()
+					{
+						Usuario = "José",
+						Senha = "123"
+					},
+					new DadosUsuario()
+					{
+						Usuario = "Maria",
+						Senha = "321"
+					}
+			};
 
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+			// LINQ
+			if(lista_usuarios.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha) ))
+			{
+
+			}
+
+		}
+		catch (Exception ex) 
+		{
+			DisplayAlert("Ops!", ex.Message, "Fechar");
+		}
     }
 }
